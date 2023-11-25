eb11c6f409b88be36a1b20d9cd9e8af9d5815570
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-11-25 21:34:44 +0600

    style- register page feat- image upload to img bb

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,16 +1,28 @@
 import { useForm } from "react-hook-form";
 import image from "../../assets/signup.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-const Login = () => {
+const image_hosting_key = import.meta.env.VITE_IMGBB_API;
+const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
+const Register = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      const imageFile = new FormData();
+      imageFile.append("image", data.image[0]);
+      const response = await axios.post(image_hosting_url, imageFile);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error during image upload:", error);
+    }
   };
   return (
     <div className="flex justify-center items-center">
@@ -18,8 +30,24 @@ const Login = () => {
         <img className="w-[75%] mx-auto" src={image} alt="" />
       </div>
       <div className="lg:w-1/2">
-        <h2 className="lg:w-2/3 text-6xl font-bold text-center">LOGIN !!</h2>
+        <h2 className="lg:w-2/3 text-4xl font-bold text-center">
+          PLEASE REGISTER !!
+        </h2>
         <form onSubmit={handleSubmit(onSubmit)} className="lg:w-2/3 mt-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-bold">Your name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="name"
+              className="input input-bordered"
+            />
+            {errors.name && (
+              <p className="text-red-600 mt-4">First name is required</p>
+            )}
+          </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text font-bold">Email</span>
@@ -58,6 +86,19 @@ const Login = () => {
               </p>
             )}
           </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-bold">Upload Image</span>
+            </label>
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full border-custom-main2"
+            />
+            {errors.image && (
+              <p className="text-red-600 mt-4">File is required</p>
+            )}
+          </div>
           <div className="form-control mt-6">
             <input
               className="btn bg-custom-main2 text-white font-semibold hover:bg-custom-main2"
@@ -67,7 +108,7 @@ const Login = () => {
           </div>
           <p className="text-black mt-4 text-center ">
             Do not have an Account?
-            <Link to="/register">
+            <Link to="/login">
               <span className="font-bold ml-2 text-custom-main2">
                 Register Here
               </span>
@@ -79,4 +120,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const Routes = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const Routes = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
