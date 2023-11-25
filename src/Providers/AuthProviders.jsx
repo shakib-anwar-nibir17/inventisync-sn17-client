059ae017cb978591652e16fb5c33690a5297a902
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-11-25 21:57:45 +0600

    feat - auth environment and firebase setup

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,5 +1,36 @@
-const AuthProviders = () => {
-  return <div></div>;
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "../Firebase/Firebase.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProviders = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("current user", currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
+AuthProviders.propTypes = {
+  children: PropTypes.node,
+};
 export default AuthProviders;
